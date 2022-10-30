7652abdf0c534ee51d05dbcee2573c5060c1293e
StefanRieger <riste2@gmx.de>
2022-10-30 12:06:20 +0100

    Initial Commit

---
+++ SimpleApp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SimpleApp
+//
+//  Created by Stefan Rieger on 30.10.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
